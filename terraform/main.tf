1ff9c881f4c3b1e404eef7511d48fbd0480dc0de
NotTo60 <ybp987@gmail.com>
2025-06-26 18:00:15 +0300

    feat(terraform/ci): add ssh keygen, instance checks, and robust CI infra-test for instance readiness

--- terraform/main.tf
+++ terraform/main.tf
@@ -73,4 +73,20 @@ resource "aws_instance" "windows" {
   tags = {
     Name = "hai-windows-ci"
   }
+}
+
+resource "null_resource" "check_linux_instance" {
+  depends_on = [aws_instance.linux]
+
+  provisioner "local-exec" {
+    command = "echo Linux instance created with IP: ${aws_instance.linux.public_ip}"
+  }
+}
+
+resource "null_resource" "check_windows_instance" {
+  depends_on = [aws_instance.windows]
+
+  provisioner "local-exec" {
+    command = "echo Windows instance created with IP: ${aws_instance.windows.public_ip}"
+  }
 } 
