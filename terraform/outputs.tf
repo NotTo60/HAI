635235bf0524a97c2f96d9c4b08b1b3e9fa98b41
NotTo60 <ybp987@gmail.com>
2025-06-27 15:50:33 +0300

    Restore Windows instance and testing: add back Windows instance, password variable, and all Windows testing in CI/CD

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,7 @@
 output "linux_ip" {
-  value = try(aws_instance.linux.public_ip, "No Linux instance created")
+  value = aws_instance.linux.public_ip
 }
 
 output "windows_ip" {
-  value = try(aws_instance.windows.public_ip, "No Windows instance created")
+  value = aws_instance.windows.public_ip
 } 
