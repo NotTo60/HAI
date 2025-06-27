5a7c3460e8209ae238d1a043b24e405387c96e4d
NotTo60 <ybp987@gmail.com>
2025-06-27 15:47:16 +0300

    Fix GitHub Actions workflow: remove Windows instance references and windows_password variable

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,7 @@
 output "linux_ip" {
-  value = aws_instance.linux.public_ip
+  value = try(aws_instance.linux.public_ip, "No Linux instance created")
 }
 
 output "windows_ip" {
-  value = "No Windows instance configured"
+  value = try(aws_instance.windows.public_ip, "No Windows instance created")
 } 
